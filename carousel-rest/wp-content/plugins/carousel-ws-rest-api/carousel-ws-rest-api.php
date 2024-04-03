23cc9520a93f498001fdbeadbd329993f19a44ad
GianlucaAntolini <antolinigianluca9@gmail.com>
2024-04-03 14:20:20 +0200

    register endpoint + get results

--- carousel-rest/wp-content/plugins/carousel-ws-rest-api/carousel-ws-rest-api.php
+++ carousel-rest/wp-content/plugins/carousel-ws-rest-api/carousel-ws-rest-api.php
@@ -1,69 +1,51 @@
-<!-- Obiettivo:
-
-- Realizzare un plugin Wordpress che implementi un web-service API REST per fornire i dati necessari per la visualizzazione di un ipotetico slider/carousel (es. https://sorgalla.com/jcarousel)
-
- 
-
-Funzionalità:
-
-- Definire e registrare un custom post-type per la gestione delle slide con almeno i seguenti campi: titolo, descrizione, immagine/video.
-
-- Implementare il web-service che restituisca al chiamante la lista delle slide in formato JSON (vedi esempio in calce)
-
- 
-
-Bonus:
-
-- Implementare la gestione multilingua per i campi testuali in Italiano e Inglese
-
- 
-
-Output richiesti
-
-- Codice sorgente, sia del plugin che di Wordpress
-
-- Dump/backup database, con dati di esempio
-
-- Breve documentazione della soluzione
-
-- Snapshot repository Git (locale utilizzato durante lo sviluppo)
-
- 
-
-Esempio dati:
-
-{
-
-       "status": "success",
-
-       "data": [
-
-             {
-
-                    "title": "Prima slide",
-
-                    "description": "Lorem ipsum",
-
-                    "img": https://sorgalla.com/jcarousel/assets/img/pic/img1.jpg
-
-             },
-
-             {
-
-                    "title": "Seconda slide",
-
-                    "description": "Lorem ipsum",
-
-                    "img": https://sorgalla.com/jcarousel/assets/img/pic/img2.jpg
-
-             }
-
-       ]
-
-} -->
 
 
 <?php
+/*
+Plugin Name: Carousel WS REST API
+Description: Plugin to provide a REST API that returns necessary data for an hypotetic carousel
+Version: 1.0
+Author: Gianluca Antolini
+*/
 
+/**
+ * Get slides data from database and return them as JSON
+ *
+ * @param array $data Options for the function.
+ * @return string|null JSON string of the slides
+ */
+function get_slides( $data ) {
+//   $posts = get_posts( array(
+//     'author' => '1',
+//   ) );
 
+//   if ( empty( $posts ) ) {
+//     return new WP_Error( 'no_author', 'Invalid author', array( 'status' => 404 ) );
+//   }
+
+  //get data from database -> table is slides and columns are id, title, description, img
+  // for now just get the data and return a string concatenating everything
+
+  global $wpdb;
+  $results = $wpdb->get_results( "SELECT * FROM slides" );
+  $slides = array();
+  foreach($results as $row){
+       $slides[] = array(
+              'id' => $row->id,
+              'title' => $row->title,
+              'description' => $row->description,
+              'img' => $row->img
+       );
+  }
+  return json_encode($slides);
+}
+
+/*http://localhost/wp-json/carousel-ws-rest-api/v1/slides/1*/
+
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'carousel-ws-rest-api/v1', '/slides', array(
+    'methods' => 'GET',
+    'callback' => 'get_slides'
+  ) );
+} );
 ?>
